693fa272f5a997628df6dbdbbaf371891bb700aa
Ross McFarland <rwmcfa1@gmail.com>
2022-03-04 11:14:07 -0800

    Rel 0.0.3 version bump & CHANGELOG update

--- octodns_azure/__init__.py
+++ octodns_azure/__init__.py
@@ -20,7 +20,7 @@ from octodns.record import Record, Update, GeoCodes
 from octodns.provider import ProviderException
 from octodns.provider.base import BaseProvider
 
-__VERSION__ = '0.0.2'
+__VERSION__ = '0.0.3'
 
 
 class AzureException(ProviderException):
